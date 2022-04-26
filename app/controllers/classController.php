b61b0965988a3d96ec214f2c68a8f4a1fed4480a
tgalton <galtontom@gmail.com>
2022-04-26 13:48:07 +0200

    effective responsive grid on class, but not adative to n° of class

--- app/controllers/classController.php
+++ app/controllers/classController.php
@@ -1,6 +1,4 @@
 <?php
-// Aside must be visible here. Nav also.
-require_once "../app/controllers/navController.php";
-require_once "../app/controllers/asideController.php";
+
 // app\controllers\asideController.php
 require_once "../app/views/class.phtml";
